e0fc5d333b52b322d58789e4cda4735084078ddb
seonho99 <pandapad10@gmail.com>
2025-05-07 10:18:57 +0900

    feat: 파이어스토어 연동 완료

--- lib/component/sign_up/sign_up_screen.dart
+++ lib/component/sign_up/sign_up_screen.dart
@@ -1,147 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:share_status/component/widget/click_button.dart';
-// import 'package:share_status/component/widget/input_field.dart';
-// import 'package:share_status/core/route/routes.dart';
-//
-// class SignUpScreen extends StatefulWidget {
-//   const SignUpScreen({super.key});
-//
-//   @override
-//   State<SignUpScreen> createState() => _SignUpScreenState();
-// }
-//
-// class _SignUpScreenState extends State<SignUpScreen> {
-//   final  _emailController = TextEditingController();
-//   final  _passwordController = TextEditingController();
-//   final  _confirmPasswordController =
-//       TextEditingController();
-//   final  _idController = TextEditingController();
-//   final  _nicknameController = TextEditingController();
-//   bool _obscurePassword = true;
-//   bool _agreeToTerms = false;
-//   final _defaultImageUrl = 'assets/image/profile.png';
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//         backgroundColor: Colors.white,
-//         title: const Text(
-//           '회원가입',
-//           style: TextStyle(
-//             fontFamily: 'Noto Sans',
-//             color: Colors.black,
-//             fontSize: 30,
-//             fontWeight: FontWeight.w600,
-//           ),
-//         ),
-//       ),
-//       body: SafeArea(
-//         child: Padding(
-//           padding: const EdgeInsets.all(20.0),
-//           child: SingleChildScrollView(
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const SizedBox(height: 20),
-//                 InputField(
-//                   label: '닉네임',
-//                   hintText: '닉네임을 입력해주세요.',
-//                   controller: _nicknameController,
-//                   keyboardType: TextInputType.emailAddress,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 InputField(
-//                   label: '이메일',
-//                   hintText: '이메일을 입력해주세요.',
-//                   controller: _emailController,
-//                   keyboardType: TextInputType.emailAddress,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 InputField(
-//                   label: '비밀번호',
-//                   hintText: '비밀번호를 입력해주세요.',
-//                   controller: _passwordController,
-//                   keyboardType: TextInputType.number,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 InputField(
-//                   label: '비밀번호 재확인',
-//                   hintText: '비밀번호를 입력해주세요.',
-//                   controller: _confirmPasswordController,
-//                   obscureText: _obscurePassword,
-//                   keyboardType: TextInputType.number,
-//                   suffixIcon: IconButton(
-//                     icon: Icon(
-//                       _obscurePassword
-//                           ? Icons.visibility_off
-//                           : Icons.visibility,
-//                     ),
-//                     onPressed: () {
-//                       setState(() {
-//                         _obscurePassword = !_obscurePassword;
-//                       });
-//                     },
-//                   ),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 InputField(
-//                   label: 'ID 입력',
-//                   hintText: 'ID를 입력해주세요.',
-//                   controller: _idController,
-//                   keyboardType: TextInputType.number,
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.symmetric(vertical: 10),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.start,
-//                     children: [
-//                       Checkbox(
-//                         value: _agreeToTerms,
-//                         onChanged: (bool? value) {
-//                           setState(() {
-//                             _agreeToTerms = value ?? false;
-//                           });
-//                         },
-//                       ),
-//                       const Text(
-//                         '회원가입 약관',
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           color: Color(0xFFA7A7A7),
-//                         ),
-//                       ),
-//                       const SizedBox(width: 10),
-//                     ],
-//                   ),
-//                 ),
-//                 ClickButton(
-//                   buttonText: '회원가입',
-//                   onPressed: () {
-//                     context.go(Routes.signIn);
-//                   },
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   void dispose() {
-//     _emailController.dispose();
-//     _passwordController.dispose();
-//     _confirmPasswordController.dispose();
-//     _idController.dispose();
-//     _nicknameController.dispose();
-//     super.dispose();
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -195,7 +51,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   label: '닉네임',
                   hintText: '닉네임을 입력해주세요.',
                   controller: _nicknameController,
-                  onChanged: viewModel.onEmailChanged,
+                  onChanged: viewModel.onNicknameChanged,
                 ),
                 const SizedBox(height: 20),
                 InputField(
@@ -203,7 +59,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   hintText: '이메일을 입력해주세요.',
                   controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
-                  onChanged: viewModel.onPasswordChanged,
+                  onChanged: viewModel.onEmailChanged,
                 ),
                 const SizedBox(height: 20),
                 InputField(
@@ -257,7 +113,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       _showError('약관에 동의해주세요.');
                       return;
                     }
-
+                    viewModel.onNicknameChanged(_nicknameController.text.trim());
+                    viewModel.onEmailChanged(_emailController.text.trim());
+                    viewModel.onPasswordChanged(_passwordController.text.trim());
+                    viewModel.onConfirmPasswordChanged(_confirmPasswordController.text.trim());
+                    viewModel.onIdChanged(_idController.text.trim());
                     viewModel.signUp(
                       imageUrl: _defaultImageUrl,
                       onSuccess: () {
