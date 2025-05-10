a1ae40396e39ae36c3219f3267b6c4e12cdc6df2
seonho99 <pandapad10@gmail.com>
2025-05-10 20:47:44 +0900

    request mapper 작성

---
+++ lib/data/mapper/follow_request_mapper.dart
@@ -0,0 +1,25 @@
+import '../dto/follow_request_dto.dart';
+
+extension FollowRequestMapper on FollowRequestDto {
+  FollowRequest toDomain() {
+    return FollowRequest(
+      fromUserId: fromUserId,
+      toUserId: toUserId,
+      toUserName: toUserName,
+      toUserImageUrl: toUserImageUrl,
+      createdAt: createdAt,
+    );
+  }
+}
+
+extension FollowRequestDomainMapper on FollowRequest {
+  FollowRequestDto toDto() {
+    return FollowRequestDto(
+      fromUserId: fromUserId,
+      toUserId: toUserId,
+      toUserName: toUserName,
+      toUserImageUrl: toUserImageUrl,
+      createdAt: createdAt,
+    );
+  }
+}
