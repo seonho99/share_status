e45c8563e14c8f02aed0af633d890b44e1f507eb
seonho99 <pandapad10@gmail.com>
2025-05-10 16:16:05 +0900

    feat: 로그인 로직 작성

--- lib/data/data_source/firestore_data_source.dart
+++ lib/data/data_source/firestore_data_source.dart
@@ -2,5 +2,24 @@ import '../dto/user_dto.dart';
 
 abstract interface class FirestoreDataSource {
   Future<void> saveUser(UserDto user);
+
   Future<bool> checkIdExists(String id);
+
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<String> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signOut();
+
+  Future<void> sendPasswordResetEmail(String email);
+
+  String? get currentUserId;
+
+  bool get isSignedIn;
 }
