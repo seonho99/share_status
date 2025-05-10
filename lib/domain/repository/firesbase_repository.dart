7e2454dee3b9701ae8f6fb60af4d511c598d4887
seonho99 <pandapad10@gmail.com>
2025-05-10 16:31:02 +0900

    feat: firebase_repositroy 코드 작성

--- lib/domain/repository/firesbase_repository.dart
+++ lib/domain/repository/firesbase_repository.dart
@@ -1,6 +1,19 @@
 import '../model/user_model.dart';
 
-abstract interface class FirestoreRepository {
+abstract interface class FirebaseRepository {
   Future<void> createUser(UserModel user);
   Future<bool> isIdAvailable(String id);
+
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signOut();
+
+  Future<void> sendPasswordResetEmail(String email);
+
+  String? get currentUserId;
+
+  bool get isSignedIn;
 }
