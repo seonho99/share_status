3e48329c4ca887b2c390b1e8731ffff81385227f
seonho99 <pandapad10@gmail.com>
2025-05-10 17:10:40 +0900

    feat: passwordreset provider 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'component/auth/password_reset/password_reset_view_model.dart';
 import 'component/auth/sign_up/sign_up_view_model.dart';
 import 'component/auth/sign_in/sign_in_view_model.dart';
 import 'data/data_source/firebase_data_source_impl.dart';
 import 'data/repository/firebase_repository_impl.dart';
 import 'domain/usecase/sign_up_usecase.dart';
 import 'domain/usecase/sign_in_use_case.dart';
+import 'domain/usecase/password_reset_usecase.dart';
 import 'domain/usecase/user_usecase.dart';
 import 'firebase_options.dart';
 
@@ -34,6 +36,11 @@ void main() async {
         ChangeNotifierProvider(
           create: (_) => SignInViewModel(
             SignInUseCase(repository),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PasswordResetViewModel(
+            PasswordResetUseCase(repository),
           ),
         ),
       ],
